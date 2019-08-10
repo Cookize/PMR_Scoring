0b2d870451d275ac09407a6fc663f87d5267e437
Cookize <zy.song@qq.com>
2019-08-10 14:56:32 +0800

    Final update

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,16 +12,20 @@ import scipy.stats as stats
 import math
 
 
+# In[2]:
+
+
 # 导入数据
 def load_data():
     print("/----------------------------------------------------/")
     print("Start loading data:")
-    #import os.path
-    #fullpath = os.path.abspath('..\data\only-score.xlsx')
-    data = pd.read_excel(open('..\data\only-score.xlsx', 'rb'))
+    data = pd.read_excel(open('../data/only-score.xlsx', 'rb'))
     data.drop(['Unnamed: 0'], axis=1, inplace=True)
     print("Finish loading data!\nShape of data:", data.shape)
     return data
+
+
+# In[3]:
 
 
 # 根据缺失数据位置进行切片，得到无缺失的评分矩阵
@@ -35,6 +42,8 @@ def drop_all_na(df):
     return ret_df
 
 
+# In[4]:
+
 
 # 丢弃完全缺失的数据，得到部分缺失的评分矩阵
 def drop_partail_na(df):
@@ -48,6 +57,9 @@ def drop_partail_na(df):
     ret_df.dropna(axis=0, how="all", inplace=True)
     print("Finish droping!\nShape of data:", ret_df.shape)
     return ret_df
+
+
+# In[5]:
 
 
 # 打印评分Series的柱状图
@@ -64,6 +76,9 @@ def print_dup_bar(data_list):
     plt.show()
 
 
+# In[6]:
+
+
 # 检查是否对自己评分出现误差
 def solve_selfish(data):
     print("/----------------------------------------------------/")
@@ -72,8 +87,10 @@ def solve_selfish(data):
     for column in data.columns:
         if not self_score[column]:
             continue
-        mean = data.loc[:,column].drop(column).mean()
-        std = data.loc[:,column].drop(column).std()
+        if data.at[column, column] == np.nan:
+            continue
+        mean = data.loc[:,column].drop(column).mean(skipna=True)
+        std = data.loc[:,column].drop(column).std(skipna=True)
         conf_intveral = stats.norm.interval(0.9, loc=mean, scale=std)
         if data.at[column, column] > conf_intveral[1]:
             data.at[column, column] = mean
@@ -86,33 +103,40 @@ def solve_selfish(data):
     return data
 
 
+# In[7]:
+
+
 # 按行/列标准化数据
 def normalize_score(data, axis=0):
-    if isinstance(data, pd.DataFrame):
-        mean = data.mean(axis=1-axis)
-        std = data.std(axis=1-axis)
-        data = data.sub(mean, axis=axis).div(std, axis=axis)
-    elif isinstance(data, pd.Series):
-        mean = data.mean(axis=1-axis)
-        std = data.std(axis=1-axis)
+    if isinstance(data, pd.DataFrame) or isinstance(data, pd.Series):
+        mean = data.mean(axis=1-axis, skipna=True)
+        std = data.std(axis=1-axis, skipna=True)
         data = data.sub(mean, axis=axis).div(std, axis=axis)
     return data
 
 
+# In[8]:
+
+
 # 按照样本反标准化数据
 def denormalize_score(data, example):
-    output = data.mean(axis=0).mul(example.std()).add(example.mean())
+    output = data.mean(axis=0, skipna=True).mul(example.std(skipna=True)).add(example.mean(skipna=True))
     return output
 
 
+# In[9]:
+
+
 # 使用减小评分人评分标准差距的策略
-def get_score(score_df_origin):
+def get_score(score_df_origin, cnsidr_nan = False):
     
-    # 丢弃所有空数据
-    score_df_lite = drop_all_na(score_df_origin)
+    if cnsidr_nan:
+        score_df_lite = drop_partail_na(score_df_origin)
+    else:
+        score_df_lite = drop_all_na(score_df_origin)
     
     # 计算源数据的数值特征
-    score_mean_origin = score_df_lite.mean(axis=0)
+    score_mean_origin = score_df_lite.mean(axis=0, skipna=cnsidr_nan)
     
     # 标准化源数据
     score_df_lite_norm = normalize_score(score_df_lite, axis=0)
@@ -129,27 +153,50 @@ def get_score(score_df_origin):
     return score_mean_origin.round(decimals=2), score_final_v1.round(decimals=2), score_final_v2.round(decimals=2)
 
 
+# In[10]:
+
+
 # 比较处理前后分数
 def evaluate(data_list, index):
     print("/----------------------------------------------------/")
     print("Start evaluating result:")
+    # 输出柱状图
+    print_dup_bar(data_list)
     
     # 输出表格
     print("Comparing score:")
     print(pd.DataFrame(data=data_list, index=index).T)
     print("/----------------------------------------------------/")
-    
     # 输出排名变化情况
     print("Comparing rank:")
     rank_list = [data.rank(ascending=False, method='min').astype('int') for data in data_list]
     print(pd.DataFrame(data=rank_list, index=index).T)
-    print("/----------------------------------------------------/")
-    
-     # 输出柱状图
-    print_dup_bar(data_list)
+
+
+# In[11]:
 
 
 if __name__ == "__main__":
     score_df = load_data()
-    score_origin, score_final_v1, score_norm_v2 = get_score(score_df)
-    evaluate([score_origin, score_final_v1, score_norm_v2], ["Origin", "Normalization", "Deselfish"])
+    print("/----------------------------------------------------/")
+    print("Solution 1: Drop all missing data:")
+    score_origin, score_final_v1, score_final_v2 = get_score(score_df, False)
+    evaluate([score_origin, score_final_v1, score_final_v2], ["Origin", "Normalization", "Deselfish"])
+    print("/----------------------------------------------------/")
+    plt.scatter(score_origin.index, score_origin.loc[:], s=50, label = '$Origin$', c = 'lightskyblue', marker='.', alpha = None, edgecolors= 'skyblue')
+    plt.scatter(score_final_v1.index, score_final_v1.loc[:], s=50, label = '$Normalization$', c = 'springgreen', marker='.', alpha = None, edgecolors= 'mediumspringgreen')
+    plt.scatter(score_final_v2.index, score_final_v2.loc[:], s=50, label = '$Final$', c = 'lightsalmon', marker='.', alpha = None, edgecolors= 'salmon')
+    plt.legend()
+    plt.show()
+    
+    print("/----------------------------------------------------/")
+    print("Solution 2: Drop partial missing data:")
+    score_origin, score_final_v1, score_norm_v2 = get_score(score_df, True)
+    evaluate([score_origin, score_final_v1, score_final_v2], ["Origin", "Normalization", "Deselfish"])
+    print("/----------------------------------------------------/")
+    plt.scatter(score_origin.index, score_origin.loc[:], s=50, label = '$Origin$', c = 'lightskyblue', marker='.', alpha = None, edgecolors= 'skyblue')
+    plt.scatter(score_final_v1.index, score_final_v1.loc[:], s=50, label = '$Normalization$', c = 'springgreen', marker='.', alpha = None, edgecolors= 'mediumspringgreen')
+    plt.scatter(score_final_v2.index, score_final_v2.loc[:], s=50, label = '$Final$', c = 'lightsalmon', marker='.', alpha = None, edgecolors= 'salmon')
+    plt.legend()
+    plt.show()
+
